c7c9bc6f5a083118dbcd2d595175b2f8b4e8821b
Zambib Yassine <zambib@hotmail.fr>
2018-11-26 16:22:05 +0100

    exo4lesson2

---
+++ javascript/lesson2/exercise4/main.js
@@ -0,0 +1,19 @@
+var position = "belleville";
+
+switch(position)
+    
+ {
+    case "Belleville":
+        console.log("tu est sur la ligne 11 ou la ligne 2")
+        break;
+
+    case "mairie des Lilas":
+        console.log("tu est sur la ligne 11")
+        break;
+
+    case "père Lachaise":
+        console.log("tu est sur la ligne 3 ou 2")
+    break;
+
+    default: "Je ne sais pas ou tu es."
+    }
