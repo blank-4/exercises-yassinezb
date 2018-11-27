67f5bf5cdc0a4e5034f850365eebee49be3623b0
Zambib Yassine <zambib@hotmail.fr>
2018-11-27 17:50:27 +0100

    exo1lesson3javascript

---
+++ javascript/lesson3/exercise1/main.js
@@ -0,0 +1,14 @@
+/* * Commençons simple : Déclarez une fonction.
+choisissez un nom pour cette fonction, elle ne doit avoir aucun arguments.
+
+* On va ensuite lui donner des instructions.
+On veut que la fonction écrive "hello world !" dans la console.
+
+* Maintenant on va appeler la fonction et regarder dans la console pour voir si tout fonctionne bien. */
+
+function laCommande() {
+    console.log("hello World"); 
+}
+
+laCommande(); 
+    
