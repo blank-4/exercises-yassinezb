13dfd1cd9e887f354b23474f09ba1a73cc409da4
Zambib Yassine <zambib@hotmail.fr>
2018-11-29 12:34:33 +0100

    exo7 lesson8js

---
+++ javascript/lesson3/exercise7/main.js
@@ -0,0 +1,20 @@
+/* Créer une fonction qui calcule l'inverse d'un nombre :
+* Elle prend en paramètre le nombre
+* Elle retourne son inverse
+
+Créer une fonciton qui calcule l'opposé d'un nombre
+* Elle prend en paramètre le nombre
+* Elle retourne son opposé
+ */
+
+function calculateInverse (nombre) {
+     return (1/nombre);
+ }
+ console.log (calculateInverse(4));
+
+function calculateOpposite (nombre) {
+    return (-1*nombre);
+}
+console.log (calculateOpposite(4));
+ 
+
