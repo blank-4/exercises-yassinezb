93777802bd22431560dfe2e0efed1c1bce78e438
Zambib Yassine <zambib@hotmail.fr>
2018-11-29 10:31:15 +0100

    exo4lesson3js

---
+++ javascript/lesson3/exercise4/main.js
@@ -0,0 +1,14 @@
+var nbr1 = 19;
+var nbr2 = 8; 
+
+function calcul (nbr1,nbr2) {
+
+console.log(Math.max(nbr1, nbr2));
+};
+
+calcul(72,23)
+
+
+
+
+
---
+++ javascript/lesson3/exercise5/main.js
@@ -0,0 +1,27 @@
+/* Créer une fonciton qui calcule l'aire d'un cercle :
+* Cette fonction prend en paramètre le rayon de ce dernier
+* Elle retourne l'aire du cercle
+
+Créer une deuxième fonciton qui calcule le périmètre d'un cercle :
+* Elle prend en paramètre le rayon du cercle
+* Elle retourne le périmètre
+
+Créer une fonction qui calcule le diamètre d'un cercle :
+* Elle prend en paramètre le rayon d'un cercle
+* Elle retourne le diamètre */
+
+function calculateRayon(rayon) {
+    var resultat = (3.14*(Math.pow(rayon,2)));
+    return resultat;
+}
+console.log (calculateRayon(45));
+
+function calculatePerimetre(rayon) {
+    var resultat2 = (2*3.14*rayon);
+    return resultat2;
+}
+console.log (calculatePerimetre(5));
+
+function calculDiametre ()
+
+
