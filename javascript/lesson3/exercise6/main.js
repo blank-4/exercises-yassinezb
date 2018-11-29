49ca79cae1e1393ef1fb3c29286ec3b2546d2f70
Zambib Yassine <zambib@hotmail.fr>
2018-11-29 12:15:04 +0100

    exo6lesson3js

--- javascript/lesson3/exercise6/main.js
+++ javascript/lesson3/exercise6/main.js
@@ -1,21 +1,33 @@
-/* Créer une fonction qui calcule la puissance d'un nombre :
-* Elle prend deux paramètre :
-  * Un nombre
-  * La puissance souhaitée
-* Elle retourne le nombre à la puissance que vous vouliez.
- */
 
- function calculatePuissance (Nombre,Puissance) {
+ /* function calculatePuissance (Nombre,Puissance) {
      var resultat = Math.pow(Nombre,Puissance);
      return resultat;
  }
+ console.log (calculatePuissance(7,2)); */
 
- console.log (calculatePuissance(7,2));
+function calculatePuissance(nombre, puissance) {
+	var result = 1;
+	if(puissance == undefined)
+		puissance = 2;
+	for(var i=1; i<=puissance; i++) {
+		result = result * nombre;
+	}
+	return result;
+}
+console.log(calculatePuissance(7,2));
+
+/* function calculatePuissance (Nombre, Puissance) {
+    for (i=Puissance; i<Puissance; i= Nombre*Nombre);
+}
+
+console.log (calculatePuissance(7,2));
+ */
 
 
-
-
-
-
-
-
+/*  var pas;
+for (pas = 0; pas < 5; pas++) {
+  // Ceci sera exécuté 5 fois
+  // À chaque éxécution, la variable "pas" augmentera de 1
+  // Lorsque'elle sera arrivée à 5, le boucle se terminera.
+  console.log("Faire un pas vers l'est");
+} */
